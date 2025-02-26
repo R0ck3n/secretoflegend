387abf94746ae7802f630052571ddcc943e4c2d0
Nicolas Gracia <r0kk3nn@gmail.com>
2025-02-26 09:13:39 +0100

    pfeat:ajout des epee

--- src/classes/weapons/Sword.java
+++ src/classes/weapons/Sword.java
@@ -2,9 +2,22 @@ package classes.weapons;
 
 import interfaces.IWarriorWeapon;
 
-public class Sword extends Weapon implements IWarriorWeapon {
-    public Sword() {
+import java.util.ArrayList;
+import java.util.Arrays;
 
+public class Sword extends Weapon implements IWarriorWeapon {
+    public Sword(String name, int physicalDamage, int magicDamage) {
+        setName(name);
+        setType("Axe");
+        setRange("Melee");
+        setPhysicalDamage(physicalDamage);
+        setMagicalDamage(magicDamage);
+        setAllowedClass(new ArrayList<>(Arrays.asList("Warrior")));
     }
+
+    public Sword(String name, int physicalDamage) {
+        this(name, physicalDamage, 0);
+    }
+
 
 }
